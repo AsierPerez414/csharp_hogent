9150428b4931ed96ba660f9238b409bcc3bed0cb
AsierPerez414 <asierperez45@gmail.com>
2021-10-21 10:45:00 +0200

    Implement dice component

---
+++ src/Domain/Dice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Dice
+    {
+        #region Fields
+        private Random _randomizer = new();
+        #endregion
+        #region Properties
+        public int Dots { get; private set; }
+        #endregion
+        #region Methods
+        public Dice()
+        {
+            Dots = 6;
+        }
+        public void Roll()
+        {
+            Dots = _randomizer.Next(1, 6 + 1);
+        }
+        #endregion
+    }
+}
---
+++ src/Domain/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Game
+    {
+        #region Fields
+        private readonly List<int> _highscores = new(); //es readonly para que no lo puedan cambiar
+        private Dice _dice1, _dice2;
+        #endregion
+        #region Properties
+        public int Eye1 => _dice1.Dots;
+        public int Eye2 => _dice2.Dots;
+        public bool HasSnakeEyes { get; }
+        public IReadOnlyList<int> HighScores => _highscores.AsReadOnly();
+        public int Total { get; private set; }
+        #endregion
+        #region Constructors
+        public Game()
+        {
+            Initialize();
+        }
+        public void Initialize()
+        {
+            _dice1 = new();
+            _dice2 = new();
+        }
+        #endregion
+        public void play()
+        {
+            _dice1.Roll();
+            _dice2.Roll();
+            if (HasSnakeEyes)
+            {
+                _highscores.Add(Total);
+                Total = 0;
+            }
+            else
+            {
+                Total += Eye1 + Eye2;
+            }
+        }
+
+        public void Restart()
+        {
+            Initialize();
+        }
+    }
+}
